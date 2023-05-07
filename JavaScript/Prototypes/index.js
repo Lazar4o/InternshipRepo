e44ee47463ebb2dc98255eb68fc35362f2bced93
Lazar4o <lazar_2003@yahoo.com>
2023-05-07 23:05:21 +0300

    JS part made with Prototypes + buttons reworked

--- JavaScript/Prototypes/index.js
+++ JavaScript/Prototypes/index.js
@@ -1,18 +1,21 @@
-let currentAmount = Number(document.querySelector('#amount').innerHTML);
-
-    function depositMoney() {
+function BankAccount(currentAmount) {
+    this.currentAmount = currentAmount;
+    document.querySelector('#amount').textContent = currentAmount; 
+    this.depositMoney = function() {
         let depositMoney = Number(document.querySelector('#deposit').value);
         currentAmount += depositMoney;
-        document.querySelector('#amount').innerHTML = currentAmount;
+        document.querySelector('#amount').textContent = currentAmount;
     }
-
-    function withdrawMoney() {
+    this.withdrawMoney = function() {
         let withdrawMoney = Number(document.querySelector('#withdraw').value);
         if (withdrawMoney > currentAmount ) {
             alert(`Error!?! ${'\n'}Did you try to withdraw more than you have?!`)
             return
         }
-
         currentAmount -= withdrawMoney;
-        document.querySelector('#amount').innerHTML = currentAmount;
+        document.querySelector('#amount').textContent = currentAmount;
     }
+}
+let person = new BankAccount(Number(prompt('How much initial money do you have?')));
+document.querySelector('#deposit-btn').addEventListener('click', person.depositMoney);
+document.querySelector('#withdraw-btn').addEventListener('click', person.withdrawMoney);
